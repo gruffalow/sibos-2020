6b8005f3b05b71ef89754fd524ff921aace03f80
huiy-t-bear <49614000+huiy-t-bear@users.noreply.github.com>
2020-10-01 11:43:07 +0100

    adding versioning to images to force changes live

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    return 'Hello {}! and also Pete!\n'.format(target)
+    return 'Hello {}! and also Pete! and Alvin and Agus too!\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
