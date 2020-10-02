c9ef50408a92978f75f97595575aff37196df00c
huiy-t-bear <49614000+huiy-t-bear@users.noreply.github.com>
2020-10-02 15:36:52 +0100

    tidy up of codebase into packages and such.

---
+++ actions/extracts.py
@@ -0,0 +1,17 @@
+import logging
+from database import connection as db_con
+import pandas as pd
+
+
+def test_extract():
+    try:
+        db = db_con.connect_database()
+
+        df = pd.read_sql("select * from messages limit 10", db)
+        data = df.to_html()
+        db_con.disconnect_database(db)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        data = "broken"
+
+    return 'Test Extract\n{0}'.format(data)
--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 import os
 import logging
 
-from flask import Flask
-import pandas as pd
-
+# Setup logging to get lots of information for speeds sake
 logging.basicConfig(level=logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
 logging.info("Starting app")
 
-from database import connection as db_con
+from flask import Flask
 
+# Import the actions to map to Flask
+from actions.extracts import test_extract
 
 app = Flask(__name__)
 
+# Setup the routing from URL to code
+app.add_url_rule('/', 'root', test_extract)
 
-@app.route('/')
-def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    try:
-        db = db_con.connect_database()
-
-        df = pd.read_sql("select * from messages limit 10", db)
-        data = df.to_html()
-        db_con.disconnect_database(db)
-    except Exception as e:
-        logging.error(e, exc_info=True)
-        data = "broken"
-
-    return 'Hello {0}! and also Pete and Alvin and Agus too!\n{1}'.format(target, data)
-
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
