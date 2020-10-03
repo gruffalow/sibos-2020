f5689aec96e7d8c7f3d65707f0a85f5d7669a9f0
huiy-t-bear <49614000+huiy-t-bear@users.noreply.github.com>
2020-10-03 20:06:12 +0100

    removed cleardown and setup just to be safe for the minute

--- processing/setup_tables.py
+++ processing/setup_tables.py
@@ -6,17 +6,17 @@ def setup_tables():
     db = db_con.connect_database()
     cursor = db.cursor()
 
-    logging.info("Dropping existing processing tables")
-    cursor.execute(
-        "DROP TABLE IF EXISTS account_relationships")
-    cursor.execute(
-        "DROP TABLE IF EXISTS processing_summary")
-
-    logging.info("Creating new processing tables")
-    cursor.execute(
-        "CREATE TABLE account_relationships (id VARCHAR(255) PRIMARY KEY, document JSON, transaction_count INT, cycle_detected BOOL, cycle_value DECIMAL(13,2))")
-    cursor.execute(
-        "CREATE TABLE processing_summary (id INT AUTO_INCREMENT PRIMARY KEY, start DATETIME, end DATETIME, start_msg_no int(11), end_msg_no int(11), processed_successfully BOOL)")
+    # logging.info("Dropping existing processing tables")
+    # cursor.execute(
+    #     "DROP TABLE IF EXISTS account_relationships")
+    # cursor.execute(
+    #     "DROP TABLE IF EXISTS processing_summary")
+    #
+    # logging.info("Creating new processing tables")
+    # cursor.execute(
+    #     "CREATE TABLE account_relationships (id VARCHAR(255) PRIMARY KEY, document JSON, transaction_count INT, cycle_detected BOOL, cycle_value DECIMAL(13,2))")
+    # cursor.execute(
+    #     "CREATE TABLE processing_summary (id INT AUTO_INCREMENT PRIMARY KEY, start DATETIME, end DATETIME, start_msg_no int(11), end_msg_no int(11), processed_successfully BOOL)")
 
     db.close()
     logging.info("processing table setup complete")
