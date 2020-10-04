261da29552cb8c7efd234c954e74f1cf71ecab7d
huiy-t-bear <49614000+huiy-t-bear@users.noreply.github.com>
2020-10-04 09:34:24 +0100

    changed display page to show the cycles detected

--- actions/extracts.py
+++ actions/extracts.py
@@ -7,7 +7,7 @@ def test_extract():
     try:
         db = db_con.connect_database()
 
-        df = pd.read_sql("select * from messages limit 10", db)
+        df = pd.read_sql("select * from account_relationships where cycle_detected > 0", db)
         data = df.to_html()
         db_con.disconnect_database(db)
     except Exception as e:
